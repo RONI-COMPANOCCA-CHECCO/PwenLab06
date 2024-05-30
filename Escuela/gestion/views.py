5c88ebac5ba1a0b7058605a3c4a320910c04e304
rcompanocca <ronicompanocca@gmail.com>
2024-05-29 20:19:52 -0500

    Editamos el archivo views.py para crear vistas que manejen los formularios

--- Escuela/gestion/views.py
+++ Escuela/gestion/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+# gestion/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm, CursoForm, NotasAlumnosPorCursoForm
+from .models import Alumno, Curso, NotasAlumnosPorCurso
+
+def nuevo_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_alumnos')
+    else:
+        form = AlumnoForm()
+    return render(request, 'gestion/nuevo_alumno.html', {'form': form})
+
+def nuevo_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_cursos')
+    else:
+        form = CursoForm()
+    return render(request, 'gestion/nuevo_curso.html', {'form': form})
+
+def nueva_nota(request):
+    if request.method == 'POST':
+        form = NotasAlumnosPorCursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_notas')
+    else:
+        form = NotasAlumnosPorCursoForm()
+    return render(request, 'gestion/nueva_nota.html', {'form': form})
+
+def lista_alumnos(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'gestion/lista_alumnos.html', {'alumnos': alumnos})
+
+def lista_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'gestion/lista_cursos.html', {'cursos': cursos})
+
+def lista_notas(request):
+    notas = NotasAlumnosPorCurso.objects.all()
+    return render(request, 'gestion/lista_notas.html', {'notas': notas})
