3786ac8d63d686390e99dff61e86a93ae89154f5
rcompanocca <ronicompanocca@gmail.com>
2024-05-29 20:05:17 -0500

    definimos el modelo Alumno

--- Escuela/gestion/models.py
+++ Escuela/gestion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
