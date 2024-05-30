5b19292d77588fbeb479f82a7aad473533226fd8
rcompanocca <ronicompanocca@gmail.com>
2024-05-29 20:06:17 -0500

    Definimos la clase Curso en models.py

--- Escuela/gestion/models.py
+++ Escuela/gestion/models.py
@@ -7,3 +7,10 @@ class Alumno(models.Model):
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+    
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
